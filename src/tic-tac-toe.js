b44b3ce2c50cef7f26e609cc9e78670e52363b97
e-a-gladysheva <e.a.gladysheva303gmail.com>
2017-02-02 19:38:14 +0200

    implement all without getWinner()

--- src/tic-tac-toe.js
+++ src/tic-tac-toe.js
@@ -1,35 +1,68 @@
 class TicTacToe {
     constructor() {
+        this._currentPlayerSymbol = 'x'; 
+        this.matrix = [];
+        for(var rowIndex = 0; rowIndex < 3; rowIndex++){
+            this.matrix.push([]);
+            this.matrix[rowIndex].push(new Array(3));
+            for(var colIndex = 0; colIndex < 3; colIndex++){
+                this.matrix[rowIndex][colIndex] = null;
+            }
+        }
 
     }
 
-    getCurrentPlayerSymbol() {
-
+    getCurrentPlayerSymbol() { 
+        return this._currentPlayerSymbol;
     }
 
-    nextTurn(rowIndex, columnIndex) {
-
+    getFieldValue(rowIndex, colIndex) {
+        return this.matrix[rowIndex][colIndex];
     }
+    
+    isDraw() {
+        if (this.noMoreTurns() == true && this.getWinner() != null) {return false;}
+        if (this.noMoreTurns() == true) {return true;} 
+        if (this.getWinner() == null) {return true;}
+        else {return false;}
+    }
+
 
     isFinished() {
-
+        if (this.getWinner() != null) {return true;}
+        if (this.isDraw() == true) {return true;}
+        else {return false;}
     }
 
     getWinner() {
+       
 
     }
 
     noMoreTurns() {
+       for (var rowIndex = 0; rowIndex < 3; rowIndex++) {
+            for (var colIndex = 0; colIndex < 3; colIndex++) {
+                if (this.getFieldValue(rowIndex, colIndex) == null) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    nextTurn(rowIndex, columnIndex) {
+        if (this.getFieldValue(rowIndex, columnIndex) == null) {
+            this.matrix[rowIndex][columnIndex] = this.getCurrentPlayerSymbol();
+            if (this._currentPlayerSymbol == 'x') {
+                this._currentPlayerSymbol = 'o';
+            }
+            else {
+                this._currentPlayerSymbol = 'x';
+            }
+        }
 
     }
 
-    isDraw() {
-
-    }
-
-    getFieldValue(rowIndex, colIndex) {
-
-    }
 }
 
 module.exports = TicTacToe;

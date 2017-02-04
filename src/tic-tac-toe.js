3a8d98878b6070b9a835d2fecaa98f153b815771
e-a-gladysheva <e.a.gladysheva303gmail.com>
2017-02-04 12:21:52 +0200

    implement getWinner

--- src/tic-tac-toe.js
+++ src/tic-tac-toe.js
@@ -35,8 +35,51 @@ class TicTacToe {
     }
 
     getWinner() {
-       
+        for (var rowIndex = 0; rowIndex < 3; rowIndex++) {
+            var count_x = 0;
+            var count_o = 0;
 
+            for(var colIndex = 0; colIndex < 3; colIndex++){
+                if (this.getFieldValue(rowIndex, colIndex) == 'x') count_x++;
+                if (this.getFieldValue(rowIndex, colIndex) == 'o') count_o++;
+            }
+
+            if (count_x == 3)  return 'x';
+            if (count_o == 3) return 'o';
+        }
+
+        for (var rowIndex = 0; rowIndex < 3; rowIndex++) {
+            var count_x = 0;
+            var count_o = 0;
+
+            for(var colIndex = 0; colIndex < 3; colIndex++){
+                if (this.getFieldValue(colIndex, rowIndex) == 'x') count_x++;
+                if (this.getFieldValue(colIndex, rowIndex) == 'o') count_o++;
+            }
+
+            if (count_x == 3)  return 'x';
+            if (count_o == 3) return 'o';
+        }
+
+        var count_x = 0;
+        var count_o = 0;
+        for (var rowIndex = 0; rowIndex < 3; rowIndex++) {
+            if (this.getFieldValue(rowIndex, rowIndex) == 'x') count_x++;
+            if (this.getFieldValue(rowIndex, rowIndex) == 'o') count_o++;
+        }        
+        if (count_x == 3)  return 'x';
+        if (count_o == 3) return 'o';
+
+        var count_x = 0;
+        var count_o = 0;
+        for (var rowIndex = 0; rowIndex < 3; rowIndex++) {
+            if (this.getFieldValue(rowIndex, 2 - rowIndex) == 'x') count_x++;
+            if (this.getFieldValue(rowIndex, 2 - rowIndex) == 'o') count_o++;
+        }        
+        if (count_x == 3)  return 'x';
+        if (count_o == 3) return 'o';
+
+        return null;
     }
 
     noMoreTurns() {
